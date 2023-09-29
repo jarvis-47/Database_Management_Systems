3a049ab04c12992553d568c9044deec9e76d618d
Parampreet Singh <130951334+jarvis-47@users.noreply.github.com>
2023-09-29 14:44:23 -0400

    Add files via upload

---
+++ Quering data using HTML and PHP/OO_q7.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DBMS Example</title>
+	<style>
+		h1 {
+			font-size: 36px;
+			margin-top: 50px;
+			text-align: left;
+		}
+		p {
+			font-size: 24px;
+			margin-top: 20px;
+			text-align: left;
+		}
+		table {
+			border: 1px solid black;
+			font-size: 24px;
+			text-align: left;
+			margin-left: 20px;
+		}
+		td, th {
+			padding: 5px;
+			border: 1px solid black;
+		}
+	</style>
+</head>
+<body>
+	<h1>CPSC 6620</h1>
+	<p>Query 7 --> Use regular expression pattern matching to show name, owner, and birth date of the pets whose names start with ‘F’ and end with ‘y’ or ‘r’ in table pet.</p>
+
+<?php
+$servername = "mysql1.cs.clemson.edu";
+$username = "paramps";
+$password = "cpsc46206620";
+$dbname = "Menagerie1";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check database connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+// Query the database
+$sql = "SELECT name, owner, birth FROM pet WHERE name;";
+$result = $conn->query($sql);
+
+// Display results as a table
+if ($result->num_rows > 0) {
+	echo "<table border='1'>"; 
+	echo "<tr>";
+	$row = $result->fetch_assoc();
+	foreach ($row as $key => $value) {
+		echo "<th>" . $key . "</th>";
+	}
+	echo "</tr>";
+	foreach ($row as $key => $value) {
+		if (empty($value)) {
+			echo "<td>NULL</td>";
+		} else {
+			echo "<td>" . $value . "</td>";
+		}
+	}
+	echo "</tr>";
+	while($row = $result->fetch_assoc()) {
+		echo "<tr>";
+		foreach ($row as $key => $value) {
+			if (empty($value)) {
+				echo "<td>NULL</td>";
+			} else {
+				echo "<td>" . $value. "</td>";
+			}
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+} else {
+	echo "No Result Returned";
+}
+
+// Display the number of rows affected
+echo "<p>" . $conn->affected_rows . " rows were affected.</p>";
+
+$conn->close();
+?>
+</body>
+</html>
---
+++ Quering data using HTML and PHP/P_q4.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DBMS Example</title>
+	<style>
+		h1 {
+			font-size: 36px;
+			text-align: left;
+			margin-top: 50px;
+		}
+		p {
+			font-size: 24px;
+			text-align: left;
+			margin-top: 20px;
+		}
+    table {
+      border: 1px solid black;
+      margin: 0 auto;
+      font-size: 24px;
+      text-align: left;
+    }
+    th {
+      font-weight: bold;
+    }
+	</style>
+</head>
+<body>
+	<h1>CPSC 6620</h1>
+	<p>Query 1 : Display the structure for pet and event tables respectively.</p>
+  <?php
+  
+// Connect to MySQL database
+$servername = "mysql1.cs.clemson.edu";
+$username = "paramps";
+$password = "cpsc46206620";
+$dbname = "Menagerie1";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Define the SQL query to describe the table
+$sql = "SELECT * FROM pet WHERE name LIKE 'b%y';";
+
+// Execute the SQL query
+$result = mysqli_query($conn, $sql);
+
+// Check if the query was successful
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+// Display the results in a table
+echo "<table border='1'>";
+echo "<tr>";
+$row = mysqli_fetch_assoc($result);
+foreach ($row as $key => $value) {
+    echo "<th>" . $key . "</th>";
+}
+echo "</tr>";
+foreach ($row as $key => $value) {
+    if (empty($value)) {
+        echo "<td>NULL</td>";
+    } else {
+        echo "<td>" . $value . "</td>";
+    }
+}
+echo "</tr>";
+while($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    foreach ($row as $key => $value) {
+        if (empty($value)) {
+            echo "<td>NULL</td>";
+        } else {
+            echo "<td>" . $value . "</td>";
+        }
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
+// Show the number of rows affected
+$num_rows = mysqli_affected_rows($conn);
+echo "<p>" . $num_rows . " rows were affected.</p>";
+
+// Close the connection
+mysqli_close($conn);
+?>
+
+
+</body>
+</html>
